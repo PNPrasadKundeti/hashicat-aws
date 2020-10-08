28adc43055589f6105eb6d71d0869c71f0f58873
PNPrasadKundeti <pnprasadkundeti@gmail.com>
2020-10-08 21:08:54 +0000

    added VPC Module9

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,19 @@
 module "vpc" {
   source  = "app.terraform.io/infosys_terraformcloud/vpc/aws"
   version = "2.57.0"
-  # insert required variables here
-  cidr_block           = var.address_space
-  enable_dns_hostnames = true
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    name = "${var.prefix}-vpc"
+    Terraform = "true"
+    Environment = "dev"
   }
-  enable_nat_gateway  = true
-  single_nat_gateway  = false
-  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
-  external_nat_ip_ids = "${aws_eip.nat.*.id}"   
 }
