2d729dfb0c54d7ccd476bf1c44d9648923f482f6
PNPrasadKundeti <pnprasadkundeti@gmail.com>
2020-10-08 20:46:12 +0000

    aded VPC module2

--- vpc.tf
+++ vpc.tf
@@ -2,9 +2,14 @@ module "vpc" {
   source  = "app.terraform.io/infosys_terraformcloud/vpc/aws"
   version = "2.57.0"
   # insert required variables here
+  cidr_block           = var.address_space
+  enable_dns_hostnames = true
 
-  enable_nat_gateway  = true
-  single_nat_gateway  = false
-  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
-  external_nat_ip_ids = "${aws_eip.nat.*.id}"   
+  tags = {
+    name = "${var.prefix}-vpc"
+  }
+  #enable_nat_gateway  = true
+  #single_nat_gateway  = false
+  #reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
+  #external_nat_ip_ids = "${aws_eip.nat.*.id}"   
 }
