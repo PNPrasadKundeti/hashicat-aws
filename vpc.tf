053ed23766c86a44d70a5184888d1ab087203a62
PNPrasadKundeti <pnprasadkundeti@gmail.com>
2020-10-09 01:35:42 +0000

    Added New VPC file

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,11 @@ module "vpc" {
   source  = "app.terraform.io/infosys_terraformcloud/vpc/aws"
   version = "2.57.0"
   # insert required variables here
-  cidr_block           = var.address_space
-  enable_dns_hostnames = true
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
 
 }
